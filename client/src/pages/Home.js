d25120150cefa132b38b484c693bed87b922b859
chaitanyasonaje <chaitanyasonaje0205@gmail.com>
2025-08-11 23:07:57 +0530

    Fix Netlify build error - Replace problematic SVG data URLs with gradient backgrounds

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
       <section className="relative overflow-hidden">
         {/* Background Elements */}
         <div className="absolute inset-0 bg-gradient-to-br from-primary-600 via-primary-700 to-primary-800">
-          <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%23ffffff" fill-opacity="0.05"%3E%3Ccircle cx="30" cy="30" r="2"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-20"></div>
+          <div className="absolute inset-0 bg-gradient-to-br from-primary-700/10 to-primary-800/10 opacity-20"></div>
         </div>
         
         <div className="relative section-padding">
@@ -235,7 +235,7 @@ const Home = () => {
 
       {/* CTA Section */}
       <section className="section-padding bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 relative overflow-hidden">
-        <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%23ffffff" fill-opacity="0.03"%3E%3Ccircle cx="30" cy="30" r="2"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-30"></div>
+        <div className="absolute inset-0 bg-gradient-to-br from-gray-800/10 to-gray-900/10 opacity-30"></div>
         
         <div className="relative max-w-4xl mx-auto text-center">
           <h2 className="heading-2 text-white mb-6">
