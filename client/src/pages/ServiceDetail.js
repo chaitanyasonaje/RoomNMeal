53144f2b204a5faa062285478d9481d212792910
chaitanyasonaje <chaitanyasonaje0205@gmail.com>
2025-09-13 18:30:41 +0530

    Fix build: Add missing ServiceDetail.js placeholder page

---
+++ client/src/pages/ServiceDetail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ServiceDetail = () => (
+  <div style={{ padding: 40, textAlign: 'center' }}>
+    <h1>Service Detail Page</h1>
+    <p>This page will show details for a specific service.</p>
+  </div>
+);
+
+export default ServiceDetail;
