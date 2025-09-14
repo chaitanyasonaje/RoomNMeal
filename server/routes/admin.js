f6360992949e37086e3cdd99bb6bae526062b9b4
chaitanyasonaje <chaitanyasonaje0205@gmail.com>
2025-09-14 17:35:42 +0530

    Revert "Fix admin routes: correct auth middleware import"
    This reverts commit 56dbd3fb122a6824ad6ae7628dc6e0d2b5b667cb.

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { auth } = require('../middlewares/auth');
+const auth = require('../middlewares/auth');
 const Payment = require('../models/Payment');
 const User = require('../models/User');
 const { convertToRupees } = require('../utils/razorpay');
