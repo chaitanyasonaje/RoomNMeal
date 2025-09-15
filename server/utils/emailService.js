cddd08cc42c4977f3a1b3def4ede2dae0920cb2d
chaitanyasonaje <chaitanyasonaje0205@gmail.com>
2025-09-15 20:13:17 +0530

    fix(email): use nodemailer.createTransport instead of non-existent createTransporter

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -8,7 +8,7 @@ class EmailService {
 
   initializeTransporter() {
     try {
-      this.transporter = nodemailer.createTransporter({
+      this.transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST || 'smtp.gmail.com',
         port: process.env.SMTP_PORT || 587,
         secure: false, // true for 465, false for other ports
